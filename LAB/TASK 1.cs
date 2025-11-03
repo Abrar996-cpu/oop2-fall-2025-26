ca9be0fffa58b1d7c41cdbf9d5cc019c2a0715e9
Abrar996-cpu <abrarjamilsadaf6@gmail.com>
2025-11-04 03:32:22 +0600

    Add files via upload

---
+++ LAB/TASK 1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+       
+        Console.Write("Enter score of first student: ");
+        double score1 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter score of second student: ");
+        double score2 = Convert.ToDouble(Console.ReadLine());
+
+        Console.Write("Enter score of third student: ");
+        double score3 = Convert.ToDouble(Console.ReadLine());
+
+       
+        double average = (score1 + score2 + score3) / 3;
+
+       
+        Console.WriteLine("Average score = {0:F2}", average);
+    }
+}
---
+++ LAB/TASK 2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        double price = 250;           
+        double discountRate = 15;     
+
+       
+        double discount = (price * discountRate) / 100;
+
+        
+        double finalPrice = price - discount;
+
+        
+        Console.WriteLine("Original Price: $" + price);
+        Console.WriteLine("Discount: " + discountRate + "%");
+        Console.WriteLine("Final Price after discount: $" + finalPrice);
+    }
+}
